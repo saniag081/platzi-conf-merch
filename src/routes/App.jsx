74cfc12609a9df30f30c543f41b1dc234d6863b7
saniag081 <sgonzalezhernandez081@gmail.com>
2021-03-15 21:12:58 -0500

    added context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,20 +8,25 @@ import Payment from '../containers/Payment'
 import Succes from '../containers/Succes'
 import NotFound from '../containers/NotFound'
 import Layout from '../components/Layout'
+import AppContext from '../context/AppContext'
+import useInitialState from '../hooks/useInitialState'
 
 export default function App() {
+	const initialState = useInitialState()
 	return (
-	<BrowserRouter>
-		<Layout>
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/checkout" component={Checkout} />
-				<Route exact path="/checkout/information" component={Information} />
-				<Route exact path="/checkout/payment" component={Payment} />
-				<Route exact path="/checkout/succes" component={Succes} />
-				<Route component={NotFound} />
-			</Switch>
-		</Layout>
-	</BrowserRouter>
+	<AppContext.Provider value={initialState}>
+		<BrowserRouter>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/checkout/information" component={Information} />
+					<Route exact path="/checkout/payment" component={Payment} />
+					<Route exact path="/checkout/succes" component={Succes} />
+					<Route component={NotFound} />
+				</Switch>
+			</Layout>
+		</BrowserRouter>
+	</AppContext.Provider>
 	)
 }
